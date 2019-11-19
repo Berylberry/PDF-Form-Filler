237a5b9fb6f2a62b152d47f781df787144d037dd
Kafui Obiatey <cherylade1@gmail.com>
2019-11-19 23:39:56 +0000

    Class creation

---
+++ PDFFormFiller/APIObject/FormData.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFFormFiller.APIObject
+{
+    public partial class FormData
+    {
+        public bool does_position_appear_on_GTO_yes { get; set; } = true;
+        public bool does_position_appear_on_GTO_no { get; set; } = false;
+        public bool? are_referred_toGTO_yes { get; set; } = true;
+        public bool? are_referred_toGTO_no { get; set; } = false;
+        public string ref_partner_org_name { get; set; } = "Chipotle";
+        public string ref_partner_f_name { get; set; } = "Terrance";
+        public string ref_partner_m_name { get; set; } = "Kwame";
+        public string ref_partner_l_name { get; set; } = "Traylor";
+        public string ref_partner_tel_no { get; set; } = "650-267-8309";
+        public string ref_partner_tel_no_ext { get; set; } = "1";
+        public string alt_ref_partner_tel_no { get; set; } = "401-406-7588";
+        public string alt_ref_partner_tel_no_ext { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+        public bool does_position_appear_on_GTO_yes { get; set; }
+    }
+}
