5a854c4ca5a809d053dc858a35fc5fb01dafaae7
Kafui Obiatey <cherylade1@gmail.com>
2019-11-20 00:16:22 +0000

    data entry

--- PDFFormFiller/APIObject/FormData.cs
+++ PDFFormFiller/APIObject/FormData.cs
@@ -19,29 +19,29 @@ namespace PDFFormFiller.APIObject
         public string ref_partner_l_name { get; set; } = "Traylor";
         public string ref_partner_tel_no { get; set; } = "650-267-8309";
         public string ref_partner_tel_no_ext { get; set; } = "1";
-        public string alt_ref_partner_tel_no { get; set; } = "401-406-7588";
-        public string alt_ref_partner_tel_no_ext { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
-        public bool does_position_appear_on_GTO_yes { get; set; }
+        public string ref_partner_alt_tel_no { get; set; } = "401-406-7588";
+        public string ref_partner_alt_tel_no_ext { get; set; }
+        public string ref_partner_fax { get; set; } = "401-285-3902";
+        public string ref_partner_email { get; set; } = "lola1985@gmail.com";
+        public bool? ref_partner_pref_lang_talk_eng { get; set; } = true;
+        public bool? ref_partner_pref_lang__talk_fr { get; set; } = true;
+        public bool? ref_partner_pref_lang_write_eng { get; set; } = true;
+        public bool? ref_partner_pref_lang_write_fr { get; set; } = true;
+        public string alt_ref_partner_f_name { get; set; } = "Joseph ";
+        public string alt_ref_partner_m_name { get; set; } = "Eben";
+        public string alt_ref_partner_l_name { get; set; } = "Elam";
+        public string alt_ref_partner_tel_no { get; set; } = "401-285-3902";
+        public string alt_ref_partner_tel_no_ext { get; set; } = "1";
+        public string alt_ref_partner_alt_tel_no { get; set; } = "401-285-3902";
+        public string alt_ref_partner_alt_tel_no_ext { get; set; } = "2";
+        public string alt_ref_partner_fax { get; set; } = "401-285-3902";
+        public string alt_ref_partner_email { get; set; } = "TerranceKTraylor@teleworm.us";
+        public bool? alt_ref_partner_pref_lang_talk_eng { get; set; } = true;
+        public bool? alt_ref_partner_pref_lang__talk_fr { get; set; } = true;
+        public bool? alt_ref_partner_pref_lang_write_eng { get; set; } = true;
+        public bool? alt_ref_partner_pref_lang_write_fr { get; set; } = true;
+        public string emp_CRAP_deduct_acc_no { get; set; } = "401-285-390-401-285";
+        public string business_legal_name { get; set; }
         public bool does_position_appear_on_GTO_yes { get; set; }
         public bool does_position_appear_on_GTO_yes { get; set; }
         public bool does_position_appear_on_GTO_yes { get; set; }
